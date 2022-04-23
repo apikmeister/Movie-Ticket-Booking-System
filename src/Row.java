39a8caf33726eda44343603b2ec61bb6326c237d
apikmeister <email@youremail.com>
2022-04-23 22:29:12 +0800

    Phase 1

--- src/Row.java
+++ src/Row.java
@@ -1,46 +1,39 @@
 import java.util.*;
 
 public class Row {
-	
-	private final ArrayList<Seat> seats;
-	private final int rowNumber;
-	private int rowClass;
-	private final int seatCount;
 
-	public Row (int rowClass, int seatCount, int rowNumber)
-	{
-		this.rowNumber = rowNumber;
-		this.rowClass = rowClass;
-		this.seatCount = seatCount;
-		seats = new ArrayList<Seat>();
-		createSeats(this.seatCount);
-	}
-	
-	public void setRowClass(int rowClass)
-	{
-		this.rowClass = rowClass;
-	}
-	
-	public int getRowClass()
-	{
-		return this.rowClass;
-	}
-	
-	public int getRowNumber()
-	{
-		return rowNumber;
-	}
-	
-	public void createSeats(int seatCount)
-	{
-		for (int i = 1; i <= seatCount; i++)
-		{
-			seats.add(new Seat(false, i));
-		}
-	}
-	
-	public ArrayList<Seat> getSeats()
-	{
-		return seats;
-	}
+    private final ArrayList<Seat> seats;
+    private final int rowNumber;
+    private final int seatCount;
+    private int rowClass;
+
+    public Row(int rowClass, int seatCount, int rowNumber) {
+        this.rowNumber = rowNumber;
+        this.rowClass = rowClass;
+        this.seatCount = seatCount;
+        seats = new ArrayList<Seat>();
+        createSeats(this.seatCount);
+    }
+
+    public int getRowClass() {
+        return this.rowClass;
+    }
+
+    public void setRowClass(int rowClass) {
+        this.rowClass = rowClass;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public void createSeats(int seatCount) {
+        for (int i = 1; i <= seatCount; i++) {
+            seats.add(new Seat(false, i));
+        }
+    }
+
+    public ArrayList<Seat> getSeats() {
+        return seats;
+    }
 }
--- src/Seat.java
+++ src/Seat.java
@@ -1,39 +1,31 @@
-
 public class Seat {
-	
-	int seatNumber;
-	boolean isReserved;
-	
-	public Seat(boolean isReserved, int seatNumber)
-	{
-		this.isReserved = isReserved;
-		this.seatNumber = seatNumber;
-	}
-	
-	
-	public void setSeatNumber(int seatNumber)
-	{
-		this.seatNumber = seatNumber;
-	}
-	
-	public int getSeatNumber()
-	{
-		return this.seatNumber;
-	}
-	
-	public boolean getReservationStatus()
-	{
-		return isReserved;
-	}
-	
-	public void reserve()
-	{
-		isReserved = true;
-	}
-	
-	public void unreserved()
-	{
-		isReserved = false;
-	}	
+
+    int seatNumber;
+    boolean isReserved;
+
+    public Seat(boolean isReserved, int seatNumber) {
+        this.isReserved = isReserved;
+        this.seatNumber = seatNumber;
+    }
+
+    public int getSeatNumber() {
+        return this.seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean getReservationStatus() {
+        return isReserved;
+    }
+
+    public void reserve() {
+        isReserved = true;
+    }
+
+    public void unreserved() {
+        isReserved = false;
+    }
 
 }
